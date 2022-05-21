b612fa531767d5a686fa93430200db971dd82637
Galprim <galprimulus@gmail.com>
2022-05-21 11:57:57 +0300

    add changes Task 47

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+void FillArrayRandom(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Random rnd = new Random();
+            array[i, j] = rnd.Next(-10, 10) + rnd.NextDouble();
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            {
+                System.Console.Write("{0:f2}", array[i, j]);
+                System.Console.Write(" ");
+            }
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.WriteLine("Введите количество строк (m)");
+int m = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов (n)");
+int n = int.Parse(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+FillArrayRandom(array);
+PrintArray(array);
